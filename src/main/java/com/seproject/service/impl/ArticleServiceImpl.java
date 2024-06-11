240a1161e0b6f5df5a35eda4ed08a54845cd53cc
wzhao510 <510273941@qq.com>
2024-06-11 15:01:34 +0800

    blog back-end first init

--- src/main/java/com/seproject/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/seproject/service/impl/ArticleServiceImpl.java
@@ -56,6 +56,25 @@ public class ArticleServiceImpl implements ArticleService {
     }
 
     @Override
+    public PageBean<Article> listAll(Integer pageNum, Integer pageSize, Integer categoryId, String state) {
+        //1.创建PageBean对象
+        PageBean<Article> pb = new PageBean<>();
+
+        //2.开启分页查询 PageHelper
+        PageHelper.startPage(pageNum,pageSize);
+
+        //3.调用mapper
+        List<Article> as = articleMapper.listAll(categoryId,state);
+        //Page中提供了方法,可以获取PageHelper分页查询后 得到的总记录条数和当前页数据
+        Page<Article> p = (Page<Article>) as;
+
+        //把数据填充到PageBean对象中
+        pb.setTotal(p.getTotal());
+        pb.setItems(p.getResult());
+        return pb;
+    }
+
+    @Override
     public Article findById(Integer id) {
         Article a = articleMapper.findById(id);
         return a;
